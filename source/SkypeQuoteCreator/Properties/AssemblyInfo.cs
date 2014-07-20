0f0a473ff92e31e7e92bb508ba7c44662480825f
Matthew King <mking@mking.io>
2014-07-20 10:24:18 +0800

    Updated publish and assembly version.

--- source/SkypeQuoteCreator/Properties/AssemblyInfo.cs
+++ source/SkypeQuoteCreator/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("3390defc-1f8d-4166-8e29-2aa1b29d1e06")]
 
-[assembly: System.Reflection.AssemblyInformationalVersion("1.0.5")]
-[assembly: System.Reflection.AssemblyVersion("1.0.5.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.5.0")]
-
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.6")]
+[assembly: System.Reflection.AssemblyVersion("1.0.6.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.6.0")]
